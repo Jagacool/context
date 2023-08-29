8577ff3d729ccc617ee576913060ec0bb1c46650
j <137029997+Jagacool@users.noreply.github.com>
2023-08-29 16:51:52 +0530

    Update EditProduct.js

--- src/EditProduct.js
+++ src/EditProduct.js
@@ -1,5 +1,4 @@
 import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { API } from "./global";
